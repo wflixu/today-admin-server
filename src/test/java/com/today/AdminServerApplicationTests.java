f6041d168ce41e6df055a424ec5dfbbef245f2e3
lixu <wflixu@88.com>
2022-12-29 23:24:30 +0800

    init

---
+++ src/test/java/com/today/AdminServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.today;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
